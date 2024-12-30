a2c43fa78193774eace61eeafa878b2978a3858f
asithamdbot <asithamdbot@gmail.com>
2024-12-31 00:06:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=y7Jy2Z6a#q4DvABTv2ODXuKxrGkoFtc4hF0wuK2Ju_BmdEV6qL_Q", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/cx9d75.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am Suranga*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
